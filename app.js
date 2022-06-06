df62148fc3a94d743e8fe08e6a52ddf8c8ffe561
Muhammad Maarig <81269436+mmaarig@users.noreply.github.com>
2022-06-06 22:59:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// for (let i = 0; i < 5; i++){
-//     console.log(`Some text ${i}`);
-// }
-// for (let i = 6; i < 11; i++){
-//     console.log(`Some text ${i}`);
-// }
-
-// function printTable(){
-
-//     let numInput = document.querySelector("#numInput").value;
-
-//     for (let i = 1; i <= 10; i++){
-//         let result = (`${numInput} x ${i} = ${numInput * (i)}`);
-//         console.log(result);
-//         let sumResultElement = document.querySelector("#sumResult");
-//         sumResultElement.innerHTML = result;
-//     }
-// }
 
 function printTable() {
     let number;
@@ -27,7 +9,7 @@ function printTable() {
     for(let i = 1; i<= number2; i++){
 
     //   result = result + "<p>"+number + "x" + i + "=" + number * i+"</p>";
-    result = (`${result} <p> ${number}x${i}=${number * i} </p>`);
+    result = (`${result} <p> ${number} x ${i} = ${number * i} </p>`);
     }
 
     document.getElementById("result").innerHTML = result;
